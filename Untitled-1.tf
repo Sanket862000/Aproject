5270ec6aac2c847bb3aefb92fbd646b92a034f4f
Sanket862000 <106148651+Sanket862000@users.noreply.github.com>
2024-10-14 21:35:51 +0530

    Update Untitled-1.tf

--- Untitled-1.tf
+++ Untitled-1.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "ap-south-1"
-  shared_credentials_file = "\Users\ASUS\.aws/credentials"
+  shared_credentials_file = ""~/.aws/credentials""
 }
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
